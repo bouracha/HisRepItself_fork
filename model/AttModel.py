21e44e6a210168a153a88450c167145f3f5b4c11
bouracha <bouracha@tcd.ie>
2020-09-07 14:14:31 -0500

    Incorporated VAE branch where gen_loss is calculated in the GCN module, and appeneded to an array. I think I can do this since it seems that the GCN module is only used once as a predictor AFTER the attention section conncatenates the dct variables with the appropriate weightings

--- model/AttModel.py
+++ model/AttModel.py
@@ -86,11 +86,11 @@ class AttModel(Module):
             dct_in_tmp = torch.matmul(dct_m[:dct_n].unsqueeze(dim=0), input_gcn).transpose(1, 2)
             dct_in_tmp = torch.cat([dct_in_tmp, dct_att_tmp], dim=-1)
             dct_out_tmp, gen_loss = self.gcn(dct_in_tmp)
-            print(dct_out_tmp.shape)
-            print(gen_loss.shape)
+            #print(dct_out_tmp.shape)
+            #print(gen_loss.shape)
             out_gcn = torch.matmul(idct_m[:, :dct_n].unsqueeze(dim=0),
                                    dct_out_tmp[:, :, :dct_n].transpose(1, 2))
-            print(out_gcn.shape)
+            #print(out_gcn.shape)
             outputs.append(out_gcn.unsqueeze(2))
             gen_losses.append(gen_loss)
             if itera > 1:
--- model/GCN.py
+++ model/GCN.py
@@ -86,48 +86,6 @@ class GC_Block(nn.Module):
                + str(self.in_features) + ' -> ' \
                + str(self.out_features) + ')'
 
-
-class GCN(nn.Module):
-    def __init__(self, input_feature, hidden_feature, p_dropout, num_stage=1, node_n=48):
-        """
-        :param input_feature: num of input feature
-        :param hidden_feature: num of hidden feature
-        :param p_dropout: drop out prob.
-        :param num_stage: number of residual blocks
-        :param node_n: number of nodes in graph
-        """
-        super(GCN, self).__init__()
-        self.num_stage = num_stage
-
-        self.gc1 = GraphConvolution(input_feature, hidden_feature, node_n=node_n)
-        self.bn1 = nn.BatchNorm1d(node_n * hidden_feature)
-
-        self.gcbs = []
-        for i in range(num_stage):
-            self.gcbs.append(GC_Block(hidden_feature, p_dropout=p_dropout, node_n=node_n))
-
-        self.gcbs = nn.ModuleList(self.gcbs)
-
-        self.gc7 = GraphConvolution(hidden_feature, input_feature, node_n=node_n)
-
-        self.do = nn.Dropout(p_dropout)
-        self.act_f = nn.Tanh()
-
-    def forward(self, x):
-        y = self.gc1(x)
-        b, n, f = y.shape
-        y = self.bn1(y.view(b, -1)).view(b, n, f)
-        y = self.act_f(y)
-        y = self.do(y)
-
-        for i in range(self.num_stage):
-            y = self.gcbs[i](y)
-
-        y = self.gc7(y)
-        y = y + x
-
-        return y
-
 class GCN(nn.Module):
     def __init__(self, input_feature, hidden_feature, p_dropout, num_stage=1, node_n=48):
         """
